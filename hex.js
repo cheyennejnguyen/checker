fa073b5f5bb7804f6327fc2b15c77071c3d51fff
cheyenne <cheyenne.nguyen@bisonok.com>
2021-10-13 19:55:35 -0500

    day 6

--- hex.js
+++ hex.js
@@ -12,7 +12,14 @@ let len = word.length;
 let start = word.substring(0, Math.floor(len / 2)).toLowerCase();
 let end = word.substring(len - Math.floor(len / 2)).toLowerCase();
 
-let flip = end.split("").reverse().join("");
+//let flip = end.split("").reverse().join("");
+
+let flip = [..end].reverse().join("");
+if(start == flip) {
+    result.innerHTML = '${word.toUpperCase} is a palindrome';
+} else {
+    result.innerHTML = '${word.toUpperCase} is NOT a palindrome';
+}
 
 alert(flip);
 }
